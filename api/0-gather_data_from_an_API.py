2f4eb75ce54a147d253894780bf72a98f5f76e9a
arlind3553 <georgetesco@yahoo.com>
2023-12-18 17:39:19 +0100

    task 1

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" my first api """
+import sys
+import requests
+import json
+if __name__ == "__main__":
+    todos_api = requests.get(
+        'https://jsonplaceholder.typicode.com/todos/')
+    user_api = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1]))
+    todo_data = todos_api.text
+    user_data = user_api.text
+    user = json.loads(user_data)
+    todo = json.loads(todo_data)
+    count = 0
+    completed_tasks = []
+    for i in todo:
+        if i['userId'] == user['id']:
+            if i['completed']:
+                completed_tasks.append(i)
+            count += 1
+    print(
+        'Employee {} is done with tasks({}/{}):'
+        .format(user['name'], len(completed_tasks), count), file = sys.stdout
+    )
+    for i in completed_tasks:
+        print('\t {}'.format(i['title']), file = sys.stdout)
