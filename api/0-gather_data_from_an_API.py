9c2d552d0f4db13bb5454910270db47d16fdb29f
arlind3553 <georgetesco@yahoo.com>
2023-12-19 07:07:00 -0800

    ASK 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,30 +4,28 @@ Model to make a request to an
 API and retrieve data
 """
 
+
 import json
 import requests
 from sys import argv
 
+
 if __name__ == "__main__":
     URL = "https://jsonplaceholder.typicode.com/"
-    user_id = int(argv[1])
+    user_id = argv[1]
+    res = requests.get(f"{URL}users/{argv[1]}")
+    res = res.json()
+    user_name = res['name']
 
-    user_req = requests.get(f"{URL}/users/{user_id}")
-    data = user_req.json()
-    user_name = data.get("name")
-
-    todos = requests.get(f"{URL}/todos/")
-    todos_data = todos.json()
-
-    completed_todos = []
-    total_todos = 0
-    for todo in todos_data:
-        if todo.get("userId") == user_id:
-            total_todos += 1
-            if todo.get("completed") is True:
-                completed_todos.append(todo)
+    res = requests.get(f"{URL}todos")
+    all_todos = res.json()
+    user_todos = [todo for todo in all_todos if todo['userId'] == int(argv[1])]
+    nr_tasks = len(user_todos)
+    completed_tasks = [completed for completed in user_todos
+                       if completed['completed'] is True]
+    completed_title = [title['title'] for title in completed_tasks]
 
     print(f"Employee {user_name} is done", end="")
-    print(f" with tasks({len(completed_todos)}/{total_todos}):")
-    for todo in completed_todos:
-        print(f"\t {todo['title']}")
+    print(f" with tasks({len(completed_tasks)}/{nr_tasks}):")
+    for title in completed_title:
+        print(f"\t {title}")
