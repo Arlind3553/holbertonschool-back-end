4702a77c932f1d05dbbd80039167afa815ddb5b6
arlind3553 <georgetesco@yahoo.com>
2023-12-19 13:07:19 +0100

    task 1

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,26 +1,31 @@
 #!/usr/bin/python3
-""" my first api """
-import sys
-import requests
+"""
+Model to make a request to an
+API and retrieve data
+"""
+
+
 import json
+import requests
+from sys import argv
+
+
 if __name__ == "__main__":
-    todos_api = requests.get(
-        'https://jsonplaceholder.typicode.com/todos/')
-    user_api = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1]))
-    todo_data = todos_api.text
-    user_data = user_api.text
-    user = json.loads(user_data)
-    todo = json.loads(todo_data)
-    count = 0
-    completed_tasks = []
-    for i in todo:
-        if i['userId'] == user['id']:
-            if i['completed']:
-                completed_tasks.append(i)
-            count += 1
-    print(
-        'Employee {} is done with tasks({}/{}):'
-        .format(user['name'], len(completed_tasks), count))
-    for i in completed_tasks:
-        print('\t {}'.format(i['title']))
+    URL = "https://jsonplaceholder.typicode.com/"
+    user_id = argv[1]
+    res = requests.get(f"{URL}users/{argv[1]}")
+    res = res.json()
+    user_name = res['name']
+
+    res = requests.get(f"{URL}todos")
+    all_todos = res.json()
+    user_todos = [todo for todo in all_todos if todo['userId'] == int(argv[1])]
+    nr_tasks = len(user_todos)
+    completed_tasks = [completed for completed in user_todos
+                       if completed['completed'] is True]
+    completed_title = [title['title'] for title in completed_tasks]
+
+    print(f"Employee {user_name} is done", end="")
+    print(f" with tasks({len(completed_tasks)}/{nr_tasks}):")
+    for title in completed_title:
+        print(f"\t {title}")
